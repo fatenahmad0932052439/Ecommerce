43915d1bd9613a1a77b68876f6a660109d42c86e
Faten Ahmad <fatenahmad0932052439@gmail.com>
2026-03-11 11:25:19 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Btmheader from './comonant/header/btmheader'
+import Topheader from './comonant/header/topheader'
+
+function App() {
+
+  return (
+    <>
+    <Topheader/>
+    <Btmheader />
+
+    </>
+  )
+}
+
+export default App;
